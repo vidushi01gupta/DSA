c4cd27895e5d1017aafc80aad93e09a4d8f56921
vidushi01gupta <guptavidushi39@gmail.com>
2025-08-09 01:33:49 +0530

    Time: 1 ms (99.89%) | Memory: 43 MB (68.26%) - LeetSync

---
+++ 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.java
@@ -0,0 +1,15 @@
+class Solution {
+    public static String removeOccurrences(String s, String part) {
+        while (s.contains(part)) {
+            int index = s.indexOf(part); // Find leftmost occurrence
+            s = s.substring(0, index) + s.substring(index + part.length());
+        }
+        return s;
+    }
+
+    public static void main(String[] args) {
+        String s = "daabcbaabcbc";
+        String part = "abc";
+        System.out.println(removeOccurrences(s, part)); 
+    }
+}
