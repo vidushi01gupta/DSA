671c4c30c8a65ea585fa3cdcae3e729417d83697
vidushi01gupta <guptavidushi39@gmail.com>
2025-10-02 23:13:08 +0530

    Time: 2 ms (25.36%) | Memory: 57.6 MB (24.11%) - LeetSync

--- 189-rotate-array/rotate-array.java
+++ 189-rotate-array/rotate-array.java
@@ -1,28 +1,29 @@
 class Solution {
     public static void rotate(int[] nums, int k) {
-        int n = nums.length;
-        k = k % n; 
-        reverse(nums, 0, n - 1);
-        reverse(nums, 0, k - 1);
-        reverse(nums, k, n - 1);
+        int n=nums.length;
+        k=k%n;
+        if(k==0){
+            return;
+        }
+        reversearray(nums,0,n-1);
+        reversearray(nums,0,k-1);
+        reversearray(nums,k,n-1);
     }
 
-    private static void reverse(int[] nums, int start, int end) {
-        while (start < end) {
-            int temp = nums[start];
-            nums[start] = nums[end];
-            nums[end] = temp;
+    public static void reversearray(int nums[], int start, int end){
+        while(start<end){
+            int temp=nums[start];
+            nums[start]=nums[end];
+            nums[end]=temp;
             start++;
             end--;
+
         }
     }
-
-    public static void main(String[] args) {
-        int[] nums = {1, 2, 3, 4, 5, 6, 7};
-        int k = 3;
-        rotate(nums, k);
-        System.out.println(Arrays.toString(nums));
-    }
+    public static void main(String[] args){
+        int nums[]={1,2,3,4,5,6,7};
+        int k=3;
+        rotate(nums,k);
         
-    
+    }
 }
