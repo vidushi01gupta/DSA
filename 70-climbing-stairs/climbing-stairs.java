fa4bd1b7acc5d1129da73836f634a480cfac48ff
vidushi01gupta <guptavidushi39@gmail.com>
2025-11-15 22:48:04 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (15.00%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.java
@@ -0,0 +1,21 @@
+class Solution {
+    public static int climbStairs(int n) {
+        if(n<=2){
+            return n;
+        }
+         int prev2=1;
+         int prev1=2;
+         for(int i=3;i<=n;i++){
+            int curr=prev1+prev2;
+            prev2=prev1;
+            prev1=curr;
+         }
+         return prev1;
+
+        
+    }
+    public static void main(String[] args){
+        int n=2;
+        climbStairs(n);
+    }
+}
